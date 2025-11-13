12a6b306610ae3776ea161988206be5ed906fd15
Ville Sysinoro <53181@edu.raseko.fi>
2025-11-13 13:26:15 +0200

    Removed example page routing code

--- app.js
+++ app.js
@@ -19,62 +19,44 @@ app.set('views', './views');
 
 const pgtools = require('./postgres-tools');
 
-// Reitti etusivulle
+// Route to home page
 app.get('/', (req, res) => {
   res.render('home', { title: 'Tervetuloa Express & Handlebars -sovellukseen!' });
 });
 
-app.get('/iconList', (req, res) => {
-  res.render('iconList');
-});
-
-// Test route
-app.get('/test', (req, res) => {
-  const data = {'testKey': 'hiihoo'};
-  pgtools.selectQuery('SELECT * FROM public.vapaana').then((resultset) => {
-    console.log(resultset.rows);
-  })
-  res.render('test', data);
-});
-
-
-// Reitit kaikille Handlebars-sivuille
-app.get('/about', (req, res) => {
-  res.render('about', { title: 'Tietoa sovelluksesta', description: 'Tämä on testisivu sovelluksen tiedoille.', author: 'AutosovellusRaseko-tiimi' });
-});
-
-//  Route to vehicle listing page: free vehicles and vehicles in use
-app.get('/vehicles', (req, res) => {
-    pgtools.getVehicleData().then((resultset) => {
-        // Lets give a key for the resultset and render it to the page
-        res.render('vehicles', { vehicleList: resultset.rows });
-    });
-    
-});
-// Route to individual vehicle page: select vehicle by register number
-app.get('/vehicleDetail', (req, res) => {
-    pgtools.getVehicleDetails2(['FNK-129']).then((resultset) => {
-        // Lets give a key for the resultset and render it to the page
-        res.render('vehicleDetail', resultset.rows[0]);
-        
-    });
-    
-  });
-// Route to diary page: all entries for all vehicles
-app.get('/diary2', (req, res) => {
-  pgtools.getDiary().then((resultset) => {
-        // Lets give a key for the resultset and render it to the page
-        res.render('diary2', { diaryData: resultset.rows });
-    });
-});
-
-// TODO: Route to vehicle listing using cards
+// Route to vehicle listing using cards
 app.get('/vehiclelist', (req, res) => {
   pgtools.getVehicleData().then((resultset) => {
     // Lets give a key for the resultset and render it to the page
     res.render('vehicleList', { vehicleList: resultset.rows });
   });
 })
+
+// Route to individual vehicle page: select vehicle by register number
+app.get('/vehicleDetail', (req, res) => {
+  let register = req.query.register
+  pgtools.getVehicleDetails([register]).then((resultset) => {
+    // Convert time stamp to user friendly string 
+    let userFriendlyTimestamp = pgtools.convertToDateTimeObject(resultset.rows[0].otto);
+    let dateTimeValue = userFriendlyTimestamp.date + ' kello ' + userFriendlyTimestamp.time
+    
+    // Change original timestamp to string value
+    resultset.rows[0].otto = dateTimeValue;
+    
+    // Render it to the page
+    res.render('vehicleDetail', resultset.rows[0]);
+    });
+  });
+
+// Route to diary page: all entries for all vehicles
+app.get('/diary', (req, res) => {
+  pgtools.getDiary().then((resultset) => {
+        // Lets give a key for the resultset and render it to the page
+        res.render('diary', { diaryData: resultset.rows });
+    });
+});
+
+
 // TODO: Route to vehicle's diary page: all entries for individual vehicle by register number
 
 // TODO: Route to vehicle's tracking page: location by register number
@@ -86,26 +68,6 @@ app.get('/diary', (req, res) => {
     { date: '2025-09-17', distance: 120, driver: 'Matti' },
     { date: '2025-09-16', distance: 85, driver: 'Maija' }
   ] });
-});
-
-app.get('/dbtest', (req, res) => {
-  res.render('dbtest', { title: 'Tietokantatesti', dbStatus: 'Yhteys OK', testValue: 42 });
-});
-
-app.get('/form', (req, res) => {
-  res.render('form', { title: 'Lomake', fields: ['Nimi', 'Sähköposti', 'Viesti'] });
-});
-
-app.get('/images', (req, res) => {
-  res.render('images', { title: 'Kuvat', images: ['kuva1.jpg', 'kuva2.jpg', 'kuva3.jpg'] });
-});
-
-app.get('/index', (req, res) => {
-  res.render('index', { title: 'Etusivu', welcome: 'Tervetuloa testietusivulle!' });
-});
-
-app.get('/svgtest', (req, res) => {
-  res.render('svgtest');
 });
 
 app.get('/locate', (req, res) => {
@@ -121,27 +83,6 @@ app.get('/vehicle', (req, res) => {
     { reg: 'FNK-129', model: 'Toyota Corolla' },
     { reg: 'FPB-343', model: 'Volkswagen Golf' }
   ] });
-});
-
-app.get('/vehicledetails-FNK-129', (req, res) => {
-  res.render('vehicledetails-FNK-129', { title: 'Ajoneuvon tiedot FNK-129', details: { reg: 'FNK-129', model: 'Toyota Corolla', year: 2020 } });
-});
-
-app.get('/vehicledetails-FPB-343', (req, res) => {
-  res.render('vehicledetails-FPB-343', { title: 'Ajoneuvon tiedot FPB-343', details: { reg: 'FPB-343', model: 'Volkswagen Golf', year: 2019 } });
-});
-
-app.get('/vehicledetails-OXZ-915', (req, res) => {
-  res.render('vehicledetails-OXZ-915', { title: 'Ajoneuvon tiedot OXZ-915', details: { reg: 'OXZ-915', model: 'Ford Focus', year: 2018 } });
-});
-
-app.get('/vehicledetails', (req, res) => {
-  res.render('vehicledetails', { title: 'Ajoneuvon tiedot', details: { reg: 'ABC-123', model: 'Testiauto', year: 2025 } });
-});
-
-app.get('/vlistFlex', (req, res) => {
-
-       res.render('vlistFlex');
 });
 
 app.listen(PORT, () => {
