527d14e3cb4477ed96fb84970023375521b1eef3
Ville Sysinoro <53181@edu.raseko.fi>
2025-11-13 16:00:40 +0200

    Started creating diary page and route to this page

--- app.js
+++ app.js
@@ -1,61 +1,103 @@
+// WEB APPLICATION FOR SERVING RASEKO'S VEHICLE LENDING DATABASE
+// =============================================================
+
+// LIBRARIES
+// =========
+
+// External libraries and modules
+// ------------------------------
+
 const express = require('express');
 const {engine} = require('express-handlebars');
 
+// Local libraries and modules
+// ---------------------------
+
+const pgtools = require('./postgres-tools');
+
+// INITIALIZATION
+// --------------
+
+// Create an express app
 const app = express();
 
+// Define a TCP port to listen: read en or use 8080 in undifined
+const PORT = process.env.PORT || 8080;
+
 // Set a folder for static files and images
-const PORT = process.env.PORT || 3000;
 app.use(express.static('public'));
 app.use('/images', express.static('public/images'));
 app.use('/icons', express.static('public/icons'));
 
-// Handlebars-konfiguraatio
+// Set templating
 app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
 app.set('views', './views');
 
-// Local libraries and modules
-// -------------------------
-
-const pgtools = require('./postgres-tools');
+// URL ROUTES
+// ----------
 
 // Route to home page
 app.get('/', (req, res) => {
   res.render('home', { title: 'Tervetuloa Express & Handlebars -sovellukseen!' });
 });
 
-// Route to vehicle listing using cards
+// Route to vehicle listing page: free vehicles and vehicles in use
 app.get('/vehiclelist', (req, res) => {
   pgtools.getVehicleData().then((resultset) => {
     // Lets give a key for the resultset and render it to the page
     res.render('vehicleList', { vehicleList: resultset.rows });
   });
-})
+});
 
 // Route to individual vehicle page: select vehicle by register number
-app.get('/vehicleDetail', (req, res) => {
-  let register = req.query.register
-  pgtools.getVehicleDetails([register]).then((resultset) => {
-    // Convert time stamp to user friendly string 
-    let userFriendlyTimestamp = pgtools.convertToDateTimeObject(resultset.rows[0].otto);
-    let dateTimeValue = userFriendlyTimestamp.date + ' kello ' + userFriendlyTimestamp.time
-    
-    // Change original timestamp to string value
-    resultset.rows[0].otto = dateTimeValue;
-    
-    // Render it to the page
-    res.render('vehicleDetail', resultset.rows[0]);
+app.get('/vehicleDetails', (req, res) => {
+  let register = req.query.register;
+    pgtools.getVehicleDetails([register]).then((resultset) => {
+
+      // Convert time stamp to user friendly string
+      let userFriendlyTimestamp = pgtools.convertToDateTimeObject(resultset.rows[0].otto);
+      console.log(userFriendlyTimestamp);
+      let dateTimeValue = userFriendlyTimestamp.date + ' kello ' + userFriendlyTimestamp.time;
+      
+      // Change original timestamp to string value
+      resultset.rows[0].otto = dateTimeValue;
+      // Render it to the page
+      res.render('vehicleDetails', resultset.rows[0]);
     });
-  });
+});
 
 // Route to diary page: all entries for all vehicles
 app.get('/diary', (req, res) => {
   pgtools.getDiary().then((resultset) => {
-        // Lets give a key for the resultset and render it to the page
-        res.render('diary', { diaryData: resultset.rows });
-    });
-});
+    // Lets give a key for the resultset and render it to the page
+    let rows = resultset.rows
+    console.log(rows[0])
+    let row = 0
+    let fromattedTake = {}
+    let formattedReturn = {}
+    for (row in rows) {
+      if (rows[row].otto == null) {
+        fromattedTake.date = '-'
+        fromattedTake.time = '-'
+      }else{
+        fromattedTake = pgtools.convertToDateTimeObject(rows[row].otto);
+      }
 
+      if (rows[row].palautus == null) {
+        formattedReturn.date = '-'
+        formattedReturn.time = '-'
+      }else{
+        formattedReturn = pgtools.convertToDateTimeObject(rows[row].palautus);
+      }
+      resultset.rows[row].otto = fromattedTake.date + ' kello ' + fromattedTake.time;
+      resultset.rows[row].palautus = formattedReturn.date + ' kello ' + formattedReturn.time;
+      console.log(rows[row].otto)
+      console.log(rows[row].palautus)
+    };
+    res.render('diary', { diaryData: rows }); 
+  });
+});
 
 // TODO: Route to vehicle's diary page: all entries for individual vehicle by register number
 
@@ -63,28 +105,6 @@ app.get('/diary', (req, res) => {
 
 // SERVER START
 // ------------
-app.get('/diary', (req, res) => {
-  res.render('diary', { title: 'diary', entries: [
-    { date: '2025-09-17', distance: 120, driver: 'Matti' },
-    { date: '2025-09-16', distance: 85, driver: 'Maija' }
-  ] });
-});
 
-app.get('/locate', (req, res) => {
-  res.render('locate', { title: 'Sijainti', location: { lat: 60.5, lng: 22.3 } });
-});
-
-app.get('/signin', (req, res) => {
-  res.render('signin', { title: 'Kirjaudu sisään', username: 'testikäyttäjä' });
-});
-
-app.get('/vehicle', (req, res) => {
-  res.render('vehicle', { title: 'Ajoneuvot', vehicles: [
-    { reg: 'FNK-129', model: 'Toyota Corolla' },
-    { reg: 'FPB-343', model: 'Volkswagen Golf' }
-  ] });
-});
-
-app.listen(PORT, () => {
-  console.log(`Serveri käynnissä portissa ${PORT}`);
-});
+app.listen(PORT);
+console.log(`Server started on port ${PORT}`)
